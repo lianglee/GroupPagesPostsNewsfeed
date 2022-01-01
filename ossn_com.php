c618cd930f02e88737049c5c6450206ac139b67b
Arsalan Shah <manactric@gmail.com>
2022-01-01 20:41:52 +0500

    Update ossn_com.php

--- ossn_com.php
+++ ossn_com.php
@@ -8,11 +8,11 @@
  * @license   OPEN SOURCE SOCIAL NETWORK LICENSE 4.0
  * @link      http://www.opensource-socialnetwork.org/licence
  */
-function group_posts_newsfeed_init(){
+function group_pages_posts_newsfeed_init(){
 		ossn_unset_hook('wall', 'getPublicPosts', 'ossn_block_strip_posts');
 
-		ossn_add_hook('wall', 'getPublicPosts', 'groups_posts_show_on_newsfeed');
-		ossn_add_hook('wall', 'templates:item', 'group_posts_show_group_name', 90);
+		ossn_add_hook('wall', 'getPublicPosts', 'groups_pages_posts_show_on_newsfeed');
+		ossn_add_hook('wall', 'templates:item', 'group_posts_show_group_name', 1000);
 }
 function group_posts_show_group_name($hook, $type, $return, $param){
 		$context = ossn_get_context();
@@ -21,10 +21,11 @@ function group_posts_show_group_name($hook, $type, $return, $param){
 		}
 		return $return;
 }
-function groups_posts_show_on_newsfeed($hook, $type, $return, $params){
+function groups_pages_posts_show_on_newsfeed($hook, $type, $return, $params){
 		$return['type'] = array(
 				'user',
 				'group',
+				'businesspage',
 		);
 		$groups = new OssnGroup();
 		$list   = $groups->getMyGroups(ossn_loggedin_user());
@@ -42,12 +43,19 @@ function groups_posts_show_on_newsfeed($hook, $type, $return, $params){
 				$return['entities_pairs'][1] = array(
 						'name'   => 'poster_guid',
 						'value'  => true,
-						'wheres' => "((emd0.value=2) OR (emd0.value=3 AND o.type='user' AND [this].value IN({$params['friends_guids']})) OR (emd0.value=1 AND o.type='group' {$groups_query}))",
+						'wheres' => "((emd0.value=2) OR (emd0.value=3 AND o.type='user' AND [this].value IN({$params['friends_guids']})) OR (emd0.value=1 AND o.type='group' {$groups_query}) OR (o.type='businesspage' AND o.owner_guid IN(SELECT DISTINCT subject_id FROM `ossn_likes` WHERE `guid` = {$params['user']->guid}  AND `type` LIKE 'business:page')))",
 				);
-				if(isset($params['user']->guid) && !empty($results)){
-						$return['wheres'][] = "((emd1.value NOT IN (SELECT DISTINCT relation_to FROM `ossn_relationships` WHERE relation_from={$params['user']->guid} AND type='userblock') AND emd1.value NOT IN (SELECT DISTINCT relation_from FROM `ossn_relationships` WHERE relation_to={$params['user']->guid} AND type='userblock')))";
-				}
+		} else {
+				unset($return['entities_pairs'][1]);
+				$return['entities_pairs'][1] = array(
+						'name'   => 'poster_guid',
+						'value'  => true,
+						'wheres' => "((emd0.value=2) OR (emd0.value=3 AND o.type='user' AND [this].value IN({$params['friends_guids']})) OR (o.type='businesspage' AND o.owner_guid IN(SELECT DISTINCT subject_id FROM `ossn_likes` WHERE `guid` = {$params['user']->guid}  AND `type` LIKE 'business:page')))",
+				);				
 		}
+		if(isset($params['user']->guid)){
+						$return['wheres'][] = "((emd1.value NOT IN (SELECT DISTINCT relation_to FROM `ossn_relationships` WHERE relation_from={$params['user']->guid} AND type='userblock') AND emd1.value NOT IN (SELECT DISTINCT relation_from FROM `ossn_relationships` WHERE relation_to={$params['user']->guid} AND type='userblock')))";
+		}		
 		return $return;
 }
-ossn_register_callback('ossn', 'init', 'group_posts_newsfeed_init');
+ossn_register_callback('ossn', 'init', 'group_pages_posts_newsfeed_init');
